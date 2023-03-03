009ade3eae14edc5184c03d8e3e488847b0325c1
kev <kevbs.jsx@gmail.com>
2023-03-03 02:48:28 -0500

    new way of use the lib

--- __test__/rcl.test.ts
+++ __test__/rcl.test.ts
@@ -1,14 +1,14 @@
-import { pokeApi } from 'kev-rcl-tsx'
+// import { pokeApi } from 'kev-rcl-tsx'
+import { pokeApi } from '../src/index'
 import { describe, expect, test } from 'vitest';
 
 describe('createApi', () => {
 
   test('should return a proxy object', async () => {
     
-    const res = await pokeApi.pokemon('1');
+    const res = await pokeApi.pokemon('get_by_id')(1);
+    console.log(res);
     
-    console.log({res});
-    
-    expect(res).toEqual({ name: 'bulbasaur' })
+    expect(res).toEqual(Object)
   });
 })
--- src/index.ts
+++ src/index.ts
@@ -5,42 +5,61 @@ const POKE_API_METHODS = ['pokemon', 'type', 'ability'] as const;
 type ValidMethods = typeof POKE_API_METHODS[number];
 
 type Params = Record<'limit' | 'offset', StrOrInt>;
-
+type Methods = 'get_all' | 'get_by_id' | 'get_by_name';
+type Check<T, S, K> = T extends 'get_all' ? S : K;
 type Props<T> = {
-  [K in keyof T]: <S extends string | Params>(args: S) => Promise<S extends string ? Main : Pokemon>;
+  [K in keyof T]: <M extends Methods>(props: M) 
+    => <S extends Check<M, Params, StrOrInt>>(args: S) 
+      => Promise<Check<M, Main, Pokemon>>;
 };
 
 type StrOrInt = string | number;
 
-function isPokemon(q: unknown): q is Pokemon {
-  if (q !== null && typeof q === 'object') return true;
+// function isPokemon(q: unknown): q is Pokemon {
+//   if (q !== null && typeof q === 'object') return true;
 
-  return false;
+//   return false;
+// }
+
+async function handleResponse<T>(url: URL): Promise<T> {
+  const res = await fetch(url.toString());
+  if (!res.ok) return Promise.reject({ error: `Something wrong happened with ${url}`});
+  const pokemon = await res.json() as T;
+
+  return pokemon;
 }
 
 const createApi = <T>(api_url: string, validProps: readonly ValidMethods[]) => {
   return new Proxy<Props<T>>({} as Props<T>, {
     get: (_, prop: ValidMethods) => {
-      
-      return async (q: string | Record<string, string>) => {
+      return (method: Methods) => {
         if (!validProps.includes(prop)) return Promise.reject({ error: `Invalid method - ${prop}` })
-        
-        const url = new URL(`${api_url}/${prop}/${typeof q === 'string' ? q: ""}`)
-        
-        if (typeof q !== 'string') for (const k in q) url.searchParams.set(`${k}`, `${q[k]}`);
-        
-        const res = await fetch(url.toString());
-        if (!res.ok) return Promise.reject({ error: `Something wrong happened with ${url}`});
-        const pokemon = await res.json();
 
-        if (isPokemon(pokemon)) return pokemon
-        
-        return pokemon as Main;
+        if (method === 'get_all') {
+          return async (q: Record<string, string>) => {
+            const url = new URL(`${api_url}/${prop}`)
+          
+            for (const k in q) url.searchParams.set(`${k}`, `${q[k]}`);
+
+            const pokemon = await handleResponse<Main>(url);
+            
+            return pokemon;
+          };
+        }
+
+        return async (q: string) => {          
+          const url = new URL(`${api_url}/${prop}/${q}`)
+          
+          const pokemon = await handleResponse<Pokemon>(url);
+
+          return pokemon;
+        }
       }
     }
   });  
 };
 
 const pokeApi = createApi<PokeMethods>(API_URL, POKE_API_METHODS);
+// pokeApi.pokemon('get_all')({ limit: 2, offset: 2 }).then(res => {});
 
 export { pokeApi };
